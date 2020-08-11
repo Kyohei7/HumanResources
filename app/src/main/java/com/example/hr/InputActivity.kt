9d6a4a7baf6df7303fe4711f02f39a1728e91ba9
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-08-11 16:07:36 +0700

    add input & profile screen

---
+++ app/src/main/java/com/example/hr/InputActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+        supportActionBar?.hide()
+
+
+    }
+}
